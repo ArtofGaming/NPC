e21feffde9254787011f6483b3d46bbc129f2a82
ArtofGaming <turner4965@gmail.com>
2022-11-11 22:11:12 -0700

    update

---
+++ SRP/Assets/Damager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    public NPC npc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        npc.TakeDamage(15);
+    }
+}
